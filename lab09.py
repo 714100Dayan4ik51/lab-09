5d5673f7e7b293fbee93936b38c57b916248e872
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-11-18 17:18:22 +0500

    Создали функцию для ввода данных и их проверки.

--- lab09.py
+++ lab09.py
@@ -3,5 +3,17 @@ import logging
 
 logging.basicConfig(filename='barrel_game.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
+def get_input():
+    while True:
+        try:
+            n = int(input("Введите количество бочек (целое положительное число): "))
+            logging.info(f"Начинаем игру с {n} бочонками.")
+            if n > 0:
+                return n
 
-
+            else:
+                logging.error('Ввели отрицаетльное число.')
+                print("Пожалуйста, введите положительное число.")
+        except ValueError:
+            logging.error('Ввели не целое число.')
+            print("Пожалуйста, введите целое число.")
